219d9c4147f7aa4be28356b23d88a0ca0abd1695
DiaSird <78157340+DiaSird@users.noreply.github.com>
2026-02-04 01:44:03 +0900

    Refactor write_word method for improved readability

--- core/cpu/src/coprocessor_15.rs
+++ core/cpu/src/coprocessor_15.rs
@@ -241,10 +241,10 @@ impl Cp15 {
 
     /// Write word to TCM/cache
     pub fn write_word(&mut self, address: u32, word: u32) {
-        self.write_byte(address, (word & 0xFF));
-        self.write_byte(address + 1, ((word >> 8) & 0xFF));
-        self.write_byte(address + 2, ((word >> 16) & 0xFF));
-        self.write_byte(address + 3, ((word >> 24) & 0xFF));
+        self.write_byte(address, word & 0xFF);
+        self.write_byte(address + 1, (word >> 8) & 0xFF);
+        self.write_byte(address + 2, (word >> 16) & 0xFF);
+        self.write_byte(address + 3, (word >> 24) & 0xFF);
     }
 
     /// Write halfword to TCM/cache
