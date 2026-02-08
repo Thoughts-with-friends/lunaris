8ae1f405a14fd7f4e71b55242152bfd387e25ec2
DiaSird <78157340+DiaSird@users.noreply.github.com>
2026-02-08 18:08:06 +0900

    feat: update application icons and modify tauri configuration Co-authored-by: SARDONYX <SARDONYX-sard@users.noreply.github.com>

--- gui/tauri/src/components/templates/Top/Top.tsx
+++ gui/tauri/src/components/templates/Top/Top.tsx
@@ -3,9 +3,11 @@
 import { Box, type SxProps, type Theme } from "@mui/material";
 import { useInjectJs } from "@/components/hooks/useInjectJs";
 import { WelcomePage } from "./WelcomePage/WelcomePage";
+import { showWindow } from "@/services/api/window";
 
 export const Top = () => {
   useInjectJs();
+  showWindow();
 
   return (
     <Box component="main" sx={pageSx}>
